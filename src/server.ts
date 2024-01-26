c4614ef669e70d66185fbb52362b75e6f01eb8ef
Victor Corassa Pinheiro <victorcorassa@gmail.com>
2024-01-26 12:26:49 -0300

    port

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,9 @@ const start = async () => {
   await app.register(routes);
 
   try {
-    await app.listen({ port: 3333 });
+    await app.listen({
+      port: process.env.PORT ? Number(process.env.PORT) : 3333,
+    });
     console.log("RODANDO APLICAÇÃO");
   } catch (e) {
     process.exit(1);
